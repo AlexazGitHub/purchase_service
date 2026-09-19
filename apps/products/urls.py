6126f039eb743c63b38b068fd16bfc4c91b63810
AlexazGitHub <ai777812@gmail.com>
2026-09-19 15:52:13 +0300

    Add product detail endpoint

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 
-from apps.products.views import ProductListView
+from apps.products.views import ProductDetailView, ProductListView
 
 app_name = "products"
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="product-list"),
+    path("<int:pk>", ProductDetailView.as_view(), name="product-detail"),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,6 +1,6 @@
 """API views приложения products."""
 
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
 
 from apps.products.filters import ProductInfoFilter
@@ -19,3 +19,15 @@ class ProductListView(ListAPIView):
     serializer_class = ProductInfoSerializer
     permission_classes = (AllowAny,)
     filterset_class = ProductInfoFilter
+
+
+class ProductDetailView(RetrieveAPIView):
+    """Детальная карточка товара в конкретном магазине."""
+
+    queryset = ProductInfo.objects.select_related(
+        "product",
+        "product__category",
+        "shop",
+    ).prefetch_related("product__parameters__parameter")
+    serializer_class = ProductInfoSerializer
+    permission_classes = (AllowAny,)
